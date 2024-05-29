aa290d47da7fdb93af1ed93689017559dc20bfd7
Filip Thuresson <filip.tthuresson@gmail.com>
2024-05-29 21:49:22 +0200

    Middleware support

--- src/BaseMiddleWare.php
+++ src/BaseMiddleWare.php
@@ -2,12 +2,10 @@
 
 namespace Azcend\Core;
 
-use JetBrains\PhpStorm\NoReturn;
-
 class BaseMiddleWare
 {
 
-    #[NoReturn]public static function redirect($url, $statusCode = 303): void
+    public static function redirect($url, $statusCode = 303): void
     {
         header('Location: ' . $url, true, $statusCode);
         die();
